3be0153a1761acb191afa3b061eae90895d975f2
Duy Hai <145411748+haitd2810@users.noreply.github.com>
2024-10-18 21:37:42 +0700

    fixing

--- ASM02_ShoppingWebsite/Pages/ProductCustomer/Index.cshtml.cs
+++ ASM02_ShoppingWebsite/Pages/ProductCustomer/Index.cshtml.cs
@@ -1,12 +1,16 @@
+using ASM02_ShoppingWebsite.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASM02_ShoppingWebsite.Pages.ProductCustomer
 {
     public class IndexModel : PageModel
     {
+        public List<Product> proList { get; set; }
         public void OnGet()
         {
+            proList = PRN221_Assignment02Context.ins.Products.Include(p => p.Category).ToList();
         }
     }
 }
